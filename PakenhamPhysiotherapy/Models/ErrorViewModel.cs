acd200dd1cea13799317a82927c5130402802033
RoshanLiu <xygenio@hotmail.com>
2022-08-26 14:35:04 +1000

    xps

---
+++ PakenhamPhysiotherapy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PakenhamPhysiotherapy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PakenhamPhysiotherapy/Models/Job.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Job
+{
+    public int JobID { get; set; }
+    [Required]
+    public string CustName { get; set; }
+
+
+    [DataType(DataType.Date)]
+    [Required]
+    public DateTime JobDateTime { get; set; }
+
+
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    public string CustPhone { get; set; }
+
+
+    public string TherapistName { get; set; }
+    public string ServiceName { get; set; }
+    public JobStatus JobStatus { get; set; }
+}
+
+public enum JobStatus
+{
+    Submitted,
+    Approved,
+    Rejected,
+    Closed
+}
