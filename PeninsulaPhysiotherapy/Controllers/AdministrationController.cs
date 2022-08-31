e21d32205de4569e9d49bbb259d9a9fb13097ab1
RoshanLiu <xygenio@hotmail.com>
2022-08-31 12:23:12 +1000

    xps

--- PeninsulaPhysiotherapy/Controllers/AdministrationController.cs
+++ PeninsulaPhysiotherapy/Controllers/AdministrationController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using NuGet.Protocol.Core.Types;
 using PeninsulaPhysiotherapy.Models;
 
 namespace PeninsulaPhysiotherapy.Controllers
@@ -8,9 +7,14 @@ namespace PeninsulaPhysiotherapy.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdministrationController(
+            RoleManager<IdentityRole> roleManager,
+            UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -48,6 +52,56 @@ namespace PeninsulaPhysiotherapy.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            var model = new EditRoleVM
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleVM model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
         }
     }
 }
