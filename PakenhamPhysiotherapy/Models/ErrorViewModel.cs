b14af6b6c044dc2752e65976b16fc527565089ad
RoshanLiu <xygenio@hotmail.com>
2022-09-05 10:00:10 +1000

    xps

--- PakenhamPhysiotherapy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PakenhamPhysiotherapy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
