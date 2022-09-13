da3ff9cf3c99572a94fecd42bee2a31cefaf9c2a
RoshanLiu <xygenio@hotmail.com>
2022-09-13 20:12:03 +1000

    xps

---
+++ PeninsulaPhysiotherapy/Migrations/20220913101114_deleteTher.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeninsulaPhysiotherapy.Migrations
+{
+    public partial class deleteTher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TherapistVM");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TherapistVM",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TherapistVM", x => x.Email);
+                });
+        }
+    }
+}
